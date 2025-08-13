7c2f567aceab977d357e5e9bfcf6c6e27f91a19e
wangbaoli <wangbaoli@sudoinfotech.com>
2025-08-13 16:24:56 +0800

    basic backup+restore version

--- restore.sh
+++ restore.sh
@@ -227,7 +227,7 @@ update_fstab() {
 	sed -i "s/$old_efi_uuid/UUID=$EFIUUID/" $MNTPT/etc/fstab
 }
 
-async_files() {
+sync_files() {
 	rsync -aAXv / $MNTPT --ignore-existing \
 		--exclude=/{proc,sys,dev} \
 		--exclude=/{etc,run,var,tmp} \
@@ -275,6 +275,6 @@ choose_disk
 get_partition_sizes "$TARGETDISK"
 confirm_and_partition "$TARGETDISK" "$EFISIZE" "$SWAPSIZE" "$ROOTSIZE"
 mount_partitions
-async_files
+sync_files
 update_boot
 reboot_liveOS
